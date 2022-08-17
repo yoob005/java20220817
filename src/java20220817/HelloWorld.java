6fabc2ed9cf06d43e77453f4a68b1c6dc5ca7c48
yoob005 <110441590+yoob005@users.noreply.github.com>
2022-08-17 17:45:57 +0900

    for문 연습함

--- src/java20220817/HelloWorld.java
+++ src/java20220817/HelloWorld.java
@@ -3,5 +3,28 @@ package java20220817;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+
+		// 구구단 2~9단 출력
+		for (int a = 2; a < 10; a++) {
+			System.out.println();
+			System.out.println("구구단" + a + "단");
+
+			for (int i = 1; i < 10; i++) {
+				System.out.println(a + " x " + i + " = " + a * i);
+
+			}
+		}
+		
+		
+		// 1~100 짝수의 합만 구하기
+		
+		int sum = 0;
+		for(int i = 0; i<=100; i++) {
+			if(i%2==0) {
+				sum = sum+i; 
+			}
+		}
+		System.out.println(sum);
+	
 	}
 }
