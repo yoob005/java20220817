8f83da941d91c5379a525bbb6d4e680797fc9520
yoob005 <110441590+yoob005@users.noreply.github.com>
2022-08-17 17:06:19 +0900

    변수 , 실수형

---
+++ src/ch02/lecture/C06Double.java
@@ -0,0 +1,24 @@
+package ch02.lecture;
+
+public class C06Double {
+	public static void main(String[] args) {
+		//실수형
+		//float(4 bytes)
+		//double(8 bytes)
+		//double이 float 보다 큰범위, 더 정교한 표현 가능
+		//실수 literal 표현은 끝에 F 붙임
+		
+		double a = 3.14;
+		double b = 0.19874917239471;
+		
+		float c = 3.14F;
+		float d = 0.19874917239471F;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		
+		double f = 0.1;
+	}
+}
