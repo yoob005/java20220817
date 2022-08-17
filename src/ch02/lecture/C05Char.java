5a00d18ddf466259eddfb58a62c93e7c6d650271
yoob005 <110441590+yoob005@users.noreply.github.com>
2022-08-17 17:06:34 +0900

    변수 : char
    유니코드

---
+++ src/ch02/lecture/C05Char.java
@@ -0,0 +1,22 @@
+package ch02.lecture;
+
+public class C05Char {
+	public static void main(String[] args) {
+		//char : 2byes
+		//character
+		// 하나의 문자 표현용
+		// 유니코드 사용
+		char a;
+		a = '가'; //문자 하나는 작은 따옴표 사용
+		
+		char b = 'A';
+		char c = 'a';
+		char d = ' ';
+//		char e = ''; //빈문자는 없음
+		String f =""; //상관없음
+		
+		char g = 65;
+		char k = '\u270d';
+		
+	}
+}
