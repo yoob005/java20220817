95f120abeb94087764d78be6a72e9abaab5851e8
yoob005 <110441590+yoob005@users.noreply.github.com>
2022-08-18 17:07:54 +0900

    문자열 연산 substring
    원하는 문자만 선택

---
+++ src/ch02/lecture/C12String.java
@@ -0,0 +1,19 @@
+package ch02.lecture;
+
+public class C12String {
+	public static void main(String[] args) {
+		String a = "javascript";
+		String b = "vscode";
+		
+		
+		int x = 0;
+		int y = 4;
+		int i = 2;
+		int j = 6;
+		
+		String c = a.substring(x,y);
+		String d = b.substring(i,j);
+		
+		System.out.println(c+d);
+	}
+}
