688ba2cdae874d7b215da821039b1b03f22291a8
yoob005 <110441590+yoob005@users.noreply.github.com>
2022-08-18 17:05:28 +0900

    논리연산자 boolean

---
+++ src/ch02/lecture/C07Boolean.java
@@ -0,0 +1,11 @@
+package ch02.lecture;
+
+public class C07Boolean {
+	public static void main(String[] args) {
+		//boolean : 논리 자료형
+		// 값 : true / false
+		
+		boolean a = true;
+		a = false;
+	}
+}
