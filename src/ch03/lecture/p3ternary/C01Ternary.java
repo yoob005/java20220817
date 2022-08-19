44a5c08ec6adacfdd42b803b19596b6f677745c8
yoob005 <110441590+yoob005@users.noreply.github.com>
2022-08-19 17:47:27 +0900

    삼항 연산자

---
+++ src/ch03/lecture/p3ternary/C01Ternary.java
@@ -0,0 +1,13 @@
+package ch03.lecture.p3ternary;
+
+public class C01Ternary {
+	public static void main(String[] args) {
+		// 삼항 연산자 ?:
+		// (1항) ? (2항) : (3항)
+		// 연산결과 1항 (true) -> 2항
+		//			1항 (false) ->3항
+		
+		System.out.println(true ? 3:2); //3
+		System.out.println(false ? 3:2); //2
+	}
+}
