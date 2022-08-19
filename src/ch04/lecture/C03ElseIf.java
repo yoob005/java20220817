19669093e98a78bc500347468c80d1c72f2b2880
yoob005 <110441590+yoob005@users.noreply.github.com>
2022-08-19 17:47:52 +0900

    조건문

---
+++ src/ch04/lecture/C03ElseIf.java
@@ -0,0 +1,21 @@
+package ch04.lecture;
+
+public class C03ElseIf {
+	public static void main(String[] args) {
+		// else if : 이전 조건이 false 일  때 다시 조건확인 후 true이면 코드블럭 실행
+		
+		System.out.println(1);
+		if(true) {
+			System.out.println(2);
+		} else if(true) {
+			System.out.println(3);
+		} else if(true) {
+			System.out.println(4);
+		}
+		System.out.println(5);
+		System.out.println(6);
+		System.out.println(7);
+		System.out.println(8);
+		
+	}
+}
