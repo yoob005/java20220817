330b885ec6b18592cdc2abc59468e835e609c544
yoob005 <110441590+yoob005@users.noreply.github.com>
2022-08-22 16:55:29 +0900

    Java while

---
+++ src/ch04/lecture/C11While.java
@@ -0,0 +1,14 @@
+package ch04.lecture;
+
+public class C11While {
+	public static void main(String[] args) {
+		System.out.println(1);
+		
+		boolean a = true;
+		while (a) {
+			System.out.println(2);
+		}
+		
+		System.out.println(3);
+	}
+}
