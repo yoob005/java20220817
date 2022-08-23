00ef2f5037c57638f2a17a2c74907a7de0e6adc0
yoob005 <110441590+yoob005@users.noreply.github.com>
2022-08-23 16:44:02 +0900

    for

---
+++ src/ch04/lecture/p01control/C07For.java
@@ -0,0 +1,30 @@
+package ch04.lecture.p01control;
+
+public class C07For {
+	public static void main(String[] args) {
+//		for(int i = 1; i > 0; i++) {
+//			System.out.println("Hello");
+		for(int i1 = 1; i1 < 6; i1++) {
+			System.out.println(i1);
+			}
+		
+		System.out.println();
+				
+		for(int i2 = 5; i2 < 11; i2++) {
+			System.out.println(i2);
+		}
+		
+		System.out.println();
+		
+		for(int i3 = 5; i3 >0; i3--) {
+			System.out.println(i3);
+		}
+		
+		System.out.println();
+		
+		for(int i4 = 10; i4 > 4; i4--) {
+			System.out.println(i4);
+		}
+	
+	}
+}
