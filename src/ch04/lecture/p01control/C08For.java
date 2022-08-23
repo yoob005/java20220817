0d7dae59d5ee71100b0194de2788430ee6e9bbcd
yoob005 <110441590+yoob005@users.noreply.github.com>
2022-08-23 16:44:14 +0900

    중첩 for

---
+++ src/ch04/lecture/p01control/C08For.java
@@ -0,0 +1,19 @@
+package ch04.lecture.p01control;
+
+public class C08For {
+	public static void main(String[] args) {
+		System.out.println(1);
+		
+		
+		for(int j = 0; j < 3; j++) {
+			System.out.println(2);
+			for(int i = 0; i < 3; i++) {
+				System.out.println(3);
+				System.out.println(4);			
+			}
+		}
+		
+		
+		System.out.println(5);
+	}
+}
---
+++ src/ch04/lecture/p01control/C10For.java
@@ -0,0 +1,8 @@
+package ch04.lecture.p01control;
+
+public class C10For {
+	public static void main(String[] args) {
+		System.out.println("======================");
+		System.out.println("----------------------");
+	}
+}
