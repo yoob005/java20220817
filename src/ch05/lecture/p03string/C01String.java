34a47982f8367c5a544177503555247d647a7a6e
yoob005 <110441590+yoob005@users.noreply.github.com>
2022-08-26 09:28:54 +0900

    문자열 참조타입

---
+++ src/ch05/lecture/p03string/C01String.java
@@ -0,0 +1,15 @@
+package ch05.lecture.p03string;
+
+public class C01String {
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 3;
+		
+		System.out.println(a==b);//true
+		
+		String s1 = "java";
+		String s2 = "java";
+		
+		System.out.println(s1==s2);
+	}
+}
