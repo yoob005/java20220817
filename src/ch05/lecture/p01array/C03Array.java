9c87ef679a63566d2b27c0f9cf33717c20d460ec
yoob005 <110441590+yoob005@users.noreply.github.com>
2022-08-26 09:25:01 +0900

    배열 new 생략

---
+++ src/ch05/lecture/p01array/C03Array.java
@@ -0,0 +1,11 @@
+package ch05.lecture.p01array;
+
+public class C03Array {
+	public static void main(String[] args) {
+		int[] arr1;
+		arr1 = new int[] {9,8,7};
+		
+		int[] arr2 = new int[] {10,20};
+		int[] arr3 = {99,100,101}; // (new, 타입[]) 생략가능
+	}
+}
