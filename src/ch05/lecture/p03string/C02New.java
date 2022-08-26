00f25e942c745b397884dd559c89123632c32694
yoob005 <110441590+yoob005@users.noreply.github.com>
2022-08-26 09:29:15 +0900

    new 연산자

---
+++ src/ch05/lecture/p03string/C02New.java
@@ -0,0 +1,12 @@
+package ch05.lecture.p03string;
+
+public class C02New {
+	public static void main(String[] args) {
+		//new 연산자 : 새 인스턴스(객체)의 참조값을 연산결과로 낸다
+		String str1 = new String("java");
+		String str2 = new String("java");
+		
+		String str3 = "java"; // new 연산자 생략하고 string literal
+		String str4 = "java"; // new 연산자 생략하고 string literal을 작성하면 이미있는 참조값 활용
+	}
+}
