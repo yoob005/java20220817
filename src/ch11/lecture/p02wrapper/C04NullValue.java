68a3fc6e5d1e6c5fc361080400e6641447426641
yoob005 <110441590+yoob005@users.noreply.github.com>
2022-09-08 17:43:37 +0900

    nullvalue

---
+++ src/ch11/lecture/p02wrapper/C04NullValue.java
@@ -0,0 +1,12 @@
+package ch11.lecture.p02wrapper;
+
+public class C04NullValue {
+	public static void main(String[] args) {
+		int a = 3;
+		Integer b = 3;
+		
+		Integer c = null;
+		
+		int e = c;
+	}
+}
