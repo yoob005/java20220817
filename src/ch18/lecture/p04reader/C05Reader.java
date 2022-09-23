e2d652abc2da8f4096d8b5982344aab35d7bddee
yoob005 <110441590+yoob005@users.noreply.github.com>
2022-09-23 12:38:43 +0900

    Reader

---
+++ src/ch18/lecture/p04reader/C05Reader.java
@@ -0,0 +1,22 @@
+package ch18.lecture.p04reader;
+
+import java.io.FileReader;
+
+public class C05Reader {
+	public static void main(String[] args) throws Exception {
+		// 409개 문자로 작성된 파일
+		String fileName = "src/ch18/lecture/p04reader/C03Reader.java";
+		FileReader fr = new FileReader(fileName);
+		
+		char[] buf = new char[100];
+		int len = 0;
+		int cnt = 0;
+		while ((len = fr.read(buf)) != -1) {
+			cnt++;
+		}
+		
+		System.out.println(cnt + "번 읽음");
+
+		fr.close();
+	}
+}
