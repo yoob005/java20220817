e3a9e9b212c746633c744f8bdbe8beb264baece8
yoob005 <110441590+yoob005@users.noreply.github.com>
2022-09-23 12:40:13 +0900

    test

--- src/java20220817/test0101010.java
+++ src/java20220817/test0101010.java
@@ -9,18 +9,53 @@ import ch11.book.s110302.Key;
 
 public class test0101010 {
 	public static void main(String[] args) {
-		int[] arr = {5,9,7,10}; 
-		int divisor = 5;
-		int[] answer = {};
-		ArrayList<Integer> integerArray2 = (ArrayList<Integer>) Arrays.stream(answer).boxed().collect(Collectors.toList());
-		ArrayList<Integer> integerArray = (ArrayList<Integer>) Arrays.stream(arr).boxed().collect(Collectors.toList());
-		for(int i : integerArray) {
-			if(i%divisor==0) {
-				integerArray2.add(i);
+		int[][] arr1 = {{1,2},{2,3}};
+		int[][] arr2 = {{3,4},{5,6}};
+		
+		arr(arr1, arr2);
+
+	}
+
+	public static boolean charAscii(String str) {
+
+		if ((str.length() != 4 || str.length() != 6)) {
+			return false;
+		} //
+
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) > 60) {
+				return false;
 			}
 		}
-		int[] result =  integerArray2.stream().mapToInt(Integer::intValue).toArray();
-		System.out.println(result[0]);
+		return true;
+	}
+
+	public static int Solu(int left, int right) {
+
+		int answer = 0;
+
+		for (int i = left; i <= right; i++) {
+			int cnt = 0;
+			for (int j = 1; j <= i; j++) {
+				if (i % j == 0) {
+					cnt++;
+				}
+			}
+			if (cnt % 2 == 0) {
+				answer += i;
+			}
+			if (cnt % 2 == 1) {
+				answer -= i;
+			}
+		}
+		return answer;
+	}
+	
+	public static int[][] arr(int[][] arr1, int[][] arr2){
+		int[][] answer = {};
+		
+		return answer;
+		
 		
 	}
 }
